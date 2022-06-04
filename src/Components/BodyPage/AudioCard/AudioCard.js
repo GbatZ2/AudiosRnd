cf1fb2d4f3a1c080573917e8f9b5253a68064cf4
GbatZ2 <71204931+GbatZ2@users.noreply.github.com>
2022-06-04 13:28:49 +0200

    Update AudioCard.js

--- src/Components/BodyPage/AudioCard/AudioCard.js
+++ src/Components/BodyPage/AudioCard/AudioCard.js
@@ -13,7 +13,7 @@ export default function AudioCard() {
 
   const randomAudio = () => {
     setValor(Math.floor(Math.random() * audios.length));
-    setTimer((Math.floor(Math.random() * 3060000))+600000 )
+    setTimer((Math.floor(Math.random() * 3060000))+800000 )
   }
 
 
@@ -25,7 +25,7 @@ export default function AudioCard() {
   if (timer!=null) {
     setInterval(() => {
       setValor(Math.floor(Math.random() * audios.length));
-      setTimer((Math.floor(Math.random() * 3060000))+600000 )
+      setTimer((Math.floor(Math.random() * 3060000))+800000 )
     }, timer);
 }
 
